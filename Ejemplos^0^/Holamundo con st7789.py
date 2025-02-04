8c22069984ea5eb210eb61b7a57ce4c46e30fe1d
cobravisualcodeorg <156994418+cobravisualcodeorg@users.noreply.github.com>
2025-02-03 20:55:32 -0500

    Holamundo con st7789.py

--- Ejemplos^0^/Holamundo con st7789.py
+++ Ejemplos^0^/Holamundo con st7789.py
@@ -1,1 +1,16 @@
+import machine
+import st7789
+import vga2_8x16 as font  
+
+spi = machine.SPI(1, baudrate=8000000, sck=machine.Pin(40), mosi=machine.Pin(41))
+
+DC = machine.Pin(11, machine.Pin.OUT)
+CS = machine.Pin(12, machine.Pin.OUT)
+BL = machine.Pin(42, machine.Pin.OUT)
+
+tft = st7789.ST7789(spi, 240, 320, dc=DC, cs=CS, backlight=BL, rotation=1)
+tft.init()
+
+tft.text(font, "Hola mundo", 130, 100, st7789.WHITE, st7789.BLACK)
+
 
